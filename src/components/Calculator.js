326cae307241948f64a18b0b0a822768269d813b
victormc13 <viktorsb14@gmail.com>
2023-08-08 23:45:55 -0500

    Add Calculator.js file with functional components

---
+++ src/components/Calculator.js
@@ -0,0 +1,32 @@
+import './Calculator.css';
+
+const CalculatorDisplay = () => (
+  <div className="calculator-display">
+    0
+  </div>
+);
+
+const CalculatorButtons = () => {
+  const buttons = [
+    { key: 'AC' }, { key: '+/-' }, { key: '%' }, { key: '÷', className: 'operator' },
+    { key: '7' }, { key: '8' }, { key: '9' }, { key: '*', className: 'operator' },
+    { key: '4' }, { key: '5' }, { key: '6' }, { key: '-', className: 'operator' },
+    { key: '1' }, { key: '2' }, { key: '3' }, { key: '+', className: 'operator' },
+    { key: '0' }, { key: '.' }, { key: '=', className: 'operator' },
+  ];
+
+  return (
+    <section className="calculator-buttons">
+      {buttons.map((button) => <button type="button" key={button} className={button.className}>{button.key}</button>)}
+    </section>
+  );
+};
+
+const Calculator = () => (
+  <div className="calculator-container">
+    <CalculatorDisplay />
+    <CalculatorButtons />
+  </div>
+);
+
+export default Calculator;
