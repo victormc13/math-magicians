93c30fef01341815b9ffe9688c7cbcad3adcc34e
victormc13 <viktorsb14@gmail.com>
2023-08-16 15:08:20 -0500

    Add rendering test for App component

---
+++ src/__test__/App.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+test('renders App', () => {
+  const app = render(<BrowserRouter><App /></BrowserRouter>);
+  expect(app).toMatchSnapshot();
+});
