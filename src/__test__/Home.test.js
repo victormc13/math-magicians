c2a56ab9b93123402770aaa010def53046afe3a9
victormc13 <viktorsb14@gmail.com>
2023-08-16 15:12:58 -0500

    Add rendering test for Home component

---
+++ src/__test__/Home.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import Home from '../components/Home';
+
+test('Should render the Home page', () => {
+  const home = render(<Home />);
+  expect(home).toMatchSnapshot();
+})
