42ac969ec87bcae7842b96d0cac0f9617e0d400f
MHNazary <mhnazary2020@gmail.com>
2023-08-17 22:08:44 +0430

    Added test for header.js

---
+++ src/__test__/Header.test.js
@@ -0,0 +1,21 @@
+// import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import Header from "../components/Header";
+
+describe("Header", () => {
+  test("Renders Header component", () => {
+    const header = render(<Header />);
+    expect(header).toMatchSnapshot();
+  });
+
+  test("Renders Header component", () => {
+    render(<Header />);
+    const element = screen.getByText("Home");
+    expect(element).toBeInTheDocument();
+  });
+
+  test("has the correct CSS class", () => {
+    const { container } = render(<Header />);
+    expect(container.querySelector(".header-container")).toBeInTheDocument();
+  });
+});
